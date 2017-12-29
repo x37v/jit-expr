8d723d5da22938a67962e05ad260f9143ff92234
Alex Norman <alex@x37v.info>
2017-12-29 11:25:51 -0800

    try to catch

--- main.cpp
+++ main.cpp
@@ -3,14 +3,22 @@
 #include "lex.yy.hpp"
 #include "parser.tab.hpp"
 
+#include <iostream>
+using std::cout;
+using std::endl;
+
 int main(int argc, char* argv[]) {
   yyscan_t scanner;
 
   auto f = fopen(argv[1], "rb");
 
-  yylex_init(&scanner);
-  yyset_in(f, scanner);
-  //yylex(scanner);
-  yyparse(scanner);
-  yylex_destroy(scanner);
+  try {
+    yylex_init(&scanner);
+    yyset_in(f, scanner);
+    yylex(scanner);
+    yyparse(scanner);
+    yylex_destroy(scanner);
+  } catch (std::runtime_error& e) {
+    cout << e.what() << endl;
+  }
 }
